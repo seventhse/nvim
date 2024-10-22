1c9abaebab47839abaae548a9e0c3d2f968e8c0b
seventhse <season.sevent@icloud.com>
2024-10-22 16:31:52 +0800

    chore: Save lazy vim config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+--
+--
+--
+--
+
+local map = LazyVim.safe_keymap_set
+
+map("i", "jk", "<esc>", { noremap = true, silent = true })
